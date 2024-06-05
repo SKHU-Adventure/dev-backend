e37d5002cbd5e7551976f953fb8c193cf29be5cc
Mujae <phs514525@nate.com>
2024-06-05 16:58:51 +0900

    imagetable수정

--- DB/skhu_adventure.sql
+++ DB/skhu_adventure.sql
@@ -23,10 +23,11 @@ DROP TABLE IF EXISTS `image`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `image` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `filename` varchar(100) DEFAULT NULL,
-  `filepath` varchar(300) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+     `id` int NOT NULL AUTO_INCREMENT,
+     `userId` bigint DEFAULT NULL,
+     `imageNumber` bigint DEFAULT NULL,
+     `uri` varchar(300) DEFAULT NULL,
+     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
