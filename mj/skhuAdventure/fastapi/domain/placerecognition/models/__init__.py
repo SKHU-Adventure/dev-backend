c1de4fd7817a8e700662112defd56185adb8140a
Mujae <38150072+Mujae@users.noreply.github.com>
2024-06-06 20:21:48 +0900

    Delete mj/skhuAdventure/fastapi directory

--- mj/skhuAdventure/fastapi/domain/placerecognition/models/__init__.py
+++
@@ -1,6 +0,0 @@
-def get_model(model, **kwargs):
-    if model == "netvlad":
-        from .netvlad import NetVLAD
-        return NetVLAD(num_clusters=64, dim=512, alpha=100)
-    else:
-        raise ValueError(f"Unknown model: {model}")
